00ea216adc1a8c6a6792057bf292a2038f49ed8e
Kazzuki <madakazuki@icloud.com>
2020-11-04 16:14:00 +0900

    wip

--- chap03/ssearch_while.py
+++ chap03/ssearch_while.py
@@ -1,1 +1,20 @@
 from typing import Any, Sequence
+
+def seq_search(a: Sequence, key: Any) -> int:
+    """シーケンスaからKeyと等価な要素を線形探索(for文)"""
+    #探索成功 → 要素を返す
+
+    #探索失敗 → −1を返す
+
+if __name__ == '__main__':
+    num = int(input("要素数："))
+
+    # 事前に配列の大きさがわかるので用意する
+    input_array = [None] * num
+
+    # コンソールから要素を格納
+    for i in range(num):
+        
+
+
+    # 判定の結果
