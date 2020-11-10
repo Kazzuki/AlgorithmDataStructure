08ca1194f9f205945f259c2a2fb86f51bb099ed2
Kazzuki <madakazuki@icloud.com>
2020-11-10 12:13:54 +0900

    1pomo page p100 to p103

--- chap03/chained_hash.py
+++ chap03/chained_hash.py
@@ -28,3 +28,60 @@ class CahinedHash:
         if isinstance(key, int):
             return key % self.capacity
         return(int(hashlib.sha256(str(key).encode()).hexdigest(), 16) % self.capacity)
+    
+    def search(self, key: Any) -> Any:
+        """キーkeyを持つ要素の探索(値の返却)"""
+        hash = self.hash_value(key)
+        p = self.table[hash]
+
+        while p is not None:
+            if p.key == key:
+                return p.value
+            p = p.next
+        return None
+    
+    def add(self, key: Any, value: Any) -> bool:
+        """キーがkeyで値がvalueの要素の追加(真偽を返却)"""
+        hash = self.hash_value(key)
+        p = self.table[hash]
+
+        while p is not None:
+            if p.key == key:
+                return False
+            p = p.next
+        temp = Node(key, value, self.table[hash])
+        self.table[hash] = temp
+        return True
+    
+    def remove(self, key: Any) -> bool:
+        """キーがkeyで値がvalueの要素を削除(真偽を返却)"""
+        hash = self.hash_value(key)
+        p = self.table[hash]
+        pp = None
+
+        while p is not None:
+            if p.key == key:
+                if pp is None:
+                    self.table[hash] = p.next
+                else:
+                    pp.next = p.next
+                return True
+            pp = p
+            p = p.next
+        return False
+
+    def dump(self) -> None:
+        """ハッシュ表をダンプ"""
+        for i in range(self.capacity):
+            p = self.table[i]
+            print(i, end='')
+            while p is not None:
+                print(f' -> {p.key} ({p.value})',end='')
+                p = p.next
+            print()
+
+# 実行
+chained =  CahinedHash(10)
+chained.dump()
+chained.add('kauzki', 23)
+chained.dump()
