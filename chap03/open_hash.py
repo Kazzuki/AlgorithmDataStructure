74aa1cc991db54390daf37ded251672e6676a408
Kazzuki <madakazuki@icloud.com>
2020-11-12 16:29:21 +0900

    2pomo page p106 to p115

--- chap03/open_hash.py
+++ chap03/open_hash.py
@@ -1,7 +1,7 @@
 # open addressing hashes
 
 from __future__ import annotations
-from typing import Any, Type
+from typing import Any
 from enum import Enum
 import hashlib
 
@@ -39,5 +39,68 @@ class OpenHash(object):
         self.capcity = capacity
         self.table = [Bucket()] * self.capcity
 
-    def hssh_value(self, key: Any) -> int:
+    def hash_value(self, key: Any) -> int:
         """generate a hash value"""
+        if isinstance(key, int):
+            return key % self.capcity
+        return (int(hashlib.md5(str(key).encode()).hexdigest(), 16))
+
+    def rehash_value(self, key: Any) -> int:
+        """Generate re-hashed values"""
+        return (self.hash_value(key) + 1) % self.capcity
+    
+    def search_node(self, key: Any) -> Any:
+        """Search for Buckets"""
+        hash = self.hash_value(key)
+        p = self.table[hash]
+        for _ in range(self.capcity):
+            if p.stat == Status.EMPTY:
+                break
+            elif p.stat == Status.OCCUPIED and p.key == key:
+                return p
+            hash = self.rehash_value(hash)
+            p = self.table[hash]
+        return None
+
+    def search(self, key: Any) -> Any:
+        p = self.search_node(key)
+        if p is not None:
+            return p.value
+        else:
+            return None
+    
+    def add(self, key: Any, value: Any) -> bool:
+        if self.search(key) is not None:
+            return False
+        
+        hash = self.hash_value(key)
+        p = self.table[hash]
+        for _ in range(self.capcity):
+            if p.stat == Status.EMPTY or p.stat == Status.DELETED:
+                self.table[hash] = Bucket(key, value, Status.OCCUPIED)
+                return True
+            hash = self.rehash_value(hash)
+            p = self.table[hash]
+        # hash table is full
+        return False
+
+    def remove(self, key: Any) -> int:
+        p = self.search_node(key)
+        if p is None:
+            return False
+        p.set_status(Status.DELETED)
+        return True
+    
+    def dump(self) -> None:
+        """dunp the hash table"""
+        for i in range(self.capcity):
+            print(f"{i:2} ", end="")
+            if self.table[i].stat == Status.OCCUPIED:
+                print(f'{self.table[i].key}（{self.table[i].value}）')
+            elif self.table[i].stat == Status.EMPTY:
+                print('--　未登録　--')
+            elif self.table[i].stat == Status.DELETED:
+                print('--　削除ずみ　--')
+                
+
+
---
+++ chap03/open_hash_test.py
@@ -0,0 +1,42 @@
+from enum import Enum
+from open_hash import OpenHash
+
+Menu = Enum('Memu', ['add', 'remove', 'search', 'dump', 'END'])
+
+def select_nemu() -> Menu:
+    s = [f'({m.value}){m.name}' for m in Menu]
+    while True:
+        print(*s, sep=' ', end='')
+        n = int(input(' : '))
+        if 1 <= n <= len(Menu):
+            return Menu(n)
+
+hash = OpenHash(13)
+
+while True:
+    menu = select_nemu()
+
+    if menu == Menu.add:
+        key = int(input('キー：　'))
+        val = input('値：')
+        if not hash.add(key, val):
+            print("can't add!")
+    
+    elif menu == Menu.remove:
+        key = int(input('キー：　'))
+        if not hash.remove(key):
+            print("can't remove!")
+    
+    elif menu == Menu.search:
+        key = int(input('キー：　'))
+        t = hash.search(key)
+        if t is not None:
+            print(f"value is <{t}>")
+        else:
+            print("cna't find it!")
+            
+    elif menu == Menu.dump:
+        hash.dump()
+    else:
+        print('see you again!!')
+        break
